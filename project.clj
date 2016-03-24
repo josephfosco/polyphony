128cb34cf45c0b5525f1b72ee96f2375ae9d23a1
Joseph Fosco <joseph@josephfosco.com>
2016-03-24 09:03:03 -0700

    updated to v1.1 in project.clj

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
 ;    You should have received a copy of the GNU General Public License
 ;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(defproject polyphony "1.0.0"
+(defproject polyphony "1.1.0"
   :description "Polyphony - a basic rule engine"
   :url "https://github.com/josephfosco/polyphony"
   :license {:name "GNU General Public License version 3"
